9e29798731bb4d4c21d7768b8f708517454d1ec1
Serge Hänni <serge.haenni@simplificator.com>
2018-07-23 14:07:02 +0200

    Added command to return IP address.

--- lib/physdash.ex
+++ lib/physdash.ex
@@ -23,16 +23,47 @@ defmodule Physdash do
 
   def handle_cast({:key_pressed, character}, state) do
     new_state = if character == "*" do
+                  String.to_integer(state.command) |> process_command
                   %{state | command: ""}
                 else
-                  %{state | command: state.command <> character}
+                  new_command = state.command <> character
+
+                  displayed_command = String.pad_trailing(new_command, 16, " ")
+                  write_line(0, "VERB")
+                  write_line(1, displayed_command)
+
+                  %{state | command: new_command}
                 end
 
-    ExLCD.move_to(1, 0)
-
-    displayed_command = String.pad_trailing(new_state.command, 16, " ")
-    ExLCD.write(displayed_command)
-
     {:noreply, new_state}
+  end
+
+  defp process_command(1) do
+    write_line(0, "IP")
+    write_line(1, ip_address)
+  end
+
+  defp process_command(command) do
+    write_line(0, "Unknown verb")
+    write_line(1, Integer.to_string(command))
+  end
+
+  defp write_line(line, text) do
+    displayed_text = String.pad_trailing(text, 16, " ")
+
+    ExLCD.move_to(line, 0)
+    ExLCD.write(displayed_text)
+  end
+
+  def ip_address do
+    ip_addresses = SystemRegistry.match(:_)
+                   |> get_in([:state, :network_interface])
+                   |> Map.values
+                   |> Enum.filter(fn interface -> Map.has_key?(interface, :ipv4_address) end)
+                   |> Enum.map(fn interface -> interface.ipv4_address end)
+
+    [ip_address | _] = ip_addresses
+
+    ip_address
   end
 end
